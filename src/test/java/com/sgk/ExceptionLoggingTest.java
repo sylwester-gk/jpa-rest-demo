a4867fb805b935bc234ad7da60a1e217b657ee66
Sylwester Gk <sylwusgk@gmail.com>
2016-10-18 16:40:51 +0200

    Exception logging test.

---
+++ src/test/java/com/sgk/ExceptionLoggingTest.java
@@ -0,0 +1,39 @@
+package com.sgk;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
+public class ExceptionLoggingTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionLoggingTest.class);
+
+    private class MyException extends Exception {
+
+        @Getter
+        private String response;
+
+        public MyException(String s, Throwable throwable, String response) {
+            super(s, throwable);
+            this.response = response;
+        }
+    }
+
+    private void throwingMethod() throws Exception {
+        throw new MyException("Exceptional message", null, "Server response: JSON: {}");
+    }
+
+
+    @Test
+    public void loggingExceptionInLastParameterTest() throws Exception {
+        try {
+            throwingMethod();
+        } catch (MyException e) {
+//            log.error("Lombok: catch exception's response: {}", e.getResponse(), e);
+            LOG.error("SLF4J Factory: catch exception's response: {}", e.getResponse(), e);
+        }
+    }
+}
